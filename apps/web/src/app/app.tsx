cd1fc1dcae905e81a50c3610d7ebeb5fee19936c
CMeyer19 <colemeyer95@outlook.com>
2022-09-25 18:00:46 +0200

    Adding MUI to the application

--- apps/web/src/app/app.tsx
+++ apps/web/src/app/app.tsx
@@ -1,5 +1,17 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import { useEffect, useState } from "react";
+import Grid from "@mui/material/Grid";
+import Box from '@mui/material/Box';
+import Paper from '@mui/material/Paper';
+import { styled } from '@mui/material/styles';
+
+const Item = styled(Paper)(({ theme }) => ({
+  backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+  ...theme.typography.body2,
+  padding: theme.spacing(1),
+  textAlign: 'center',
+  color: theme.palette.text.secondary,
+}));
 
 export function App() {
   const [message, setMessage] = useState('');
@@ -17,7 +29,18 @@ export function App() {
   }, []);
 
   return (
-    <p>{message}</p>
+    <Box sx={{ flexGrow: 1 }}>
+      <p>{message}</p>
+      
+      <Grid container spacing={2}>
+        <Grid xs={4}>
+          <Item>xs=4</Item>
+        </Grid>
+        <Grid xs={8}>
+          <Item>xs=8</Item>
+        </Grid>
+      </Grid>
+    </Box>
   );
 }
 
