aa967076d5c455d77619866c95c1b52c06bce6c7
CMeyer19 <colemeyer95@outlook.com>
2022-10-06 23:52:01 +0200

    Server side Gateways code cleanup

--- apps/server/src/app/gateways/events.gateway.ts
+++ apps/server/src/app/gateways/events.gateway.ts
@@ -1,6 +1,4 @@
-import { MessageBody, SubscribeMessage, WebSocketGateway, WebSocketServer, WsResponse, } from '@nestjs/websockets';
-import { from, Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
+import { WebSocketGateway, WebSocketServer, } from '@nestjs/websockets';
 import { Server } from 'socket.io';
 
 @WebSocketGateway({
@@ -11,16 +9,6 @@ import { Server } from 'socket.io';
 export class EventsGateway {
   @WebSocketServer()
   server: Server;
-
-  @SubscribeMessage('events')
-  findAll(@MessageBody() data: any): Observable<WsResponse<number>> {
-    return from([1, 2, 3]).pipe(map(item => ({ event: 'events', data: item })));
-  }
-
-  @SubscribeMessage('identity')
-  async identity(@MessageBody() data: number): Promise<number> {
-    return data;
-  }
 
   public sendMessageEvent(message: any): void {
     this.server.emit('messages', message);
