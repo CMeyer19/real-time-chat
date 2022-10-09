31e35f706f7493c5b15fbc69c1743a81787b5a52
CMeyer19 <colemeyer95@outlook.com>
2022-10-09 20:36:31 +0200

    Refining Register screen

--- apps/web/src/app/pages/Register/RegisterPage.tsx
+++ apps/web/src/app/pages/Register/RegisterPage.tsx
@@ -1,29 +1,128 @@
 import * as React from 'react';
-import { useRef } from 'react';
+import { useEffect, useReducer, useState } from 'react';
 import { Card, CardActions, CardContent, TextField } from "@mui/material";
 import Button from "@mui/material/Button";
 import { registerUser_async } from "@real-time-chat/util-shared/auth/auth.service";
 import axios from "axios";
 import { baseApiRoute } from "@real-time-chat/util-api/features/users";
 import { IAddUserDto } from "@real-time-chat/util-api/features/users/abstractions/user.dto";
+import { useNavigate } from "react-router-dom";
+
+enum FormInputActionKind {
+  userInput
+}
+
+interface IFormInputAction {
+  type: FormInputActionKind;
+  payload: string;
+}
+
+interface IFormInputState {
+  value: string;
+  isValid: boolean;
+  validationError: string | null;
+}
+
+class FormInputState implements IFormInputState {
+  value = '';
+  isValid = false;
+  validationError = null;
+}
+
+const emailReducer = (state: IFormInputState, action: IFormInputAction): IFormInputState => {
+  switch (action.type) {
+    case FormInputActionKind.userInput: {
+      const value = action.payload;
+      const isValid = value.includes('@');
+
+      return {
+        value,
+        isValid,
+        validationError: isValid ? null : 'Your email requires the @ char'
+      };
+    }
+    default:
+      return new FormInputState();
+  }
+}
+
+const passwordReducer = (state: IFormInputState, action: IFormInputAction) => {
+  switch (action.type) {
+    case FormInputActionKind.userInput: {
+      const value = action.payload;
+      const isValid = value.length >= 6;
+
+      return {
+        value,
+        isValid,
+        validationError: isValid ? null : 'The password must be a minimum of 6 characters long'
+      };
+    }
+    default:
+      return new FormInputState();
+  }
+}
+
+const usernameReducer = (state: IFormInputState, action: IFormInputAction) => {
+  switch (action.type) {
+    case FormInputActionKind.userInput: {
+      const value = action.payload;
+      const isValid = value.length >= 6;
+
+      return {
+        value,
+        isValid,
+        validationError: isValid ? null : 'The username must be a minimum of 6 characters long'
+      };
+    }
+    default:
+      return new FormInputState();
+  }
+}
 
 const RegisterPage = () => {
-  const emailInputRef = useRef<HTMLInputElement>();
-  const usernameInputRef = useRef<HTMLInputElement>();
-  const passwordInputRef = useRef<HTMLInputElement>();
+  const [
+    {
+      value: username,
+      isValid: isUsernameValid,
+      validationError: usernameValidationError
+    },
+    dispatchUsernameAction
+  ] = useReducer(usernameReducer, new FormInputState());
+  const [
+    {
+      value: email,
+      isValid: isEmailValid,
+      validationError: emailValidationError
+    },
+    dispatchEmailAction
+  ] = useReducer(emailReducer, new FormInputState());
+  const [
+    {
+      value: password,
+      isValid: isPasswordValid,
+      validationError: passwordValidationError
+    },
+    dispatchPasswordAction
+  ] = useReducer(passwordReducer, new FormInputState());
+
+  const [isFormValid, setIsFormValid] = useState(false);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    setIsFormValid(isEmailValid && isUsernameValid && isPasswordValid);
+  }, [isEmailValid, isUsernameValid, isPasswordValid]);
 
   const createUserHandler = async (event: React.SyntheticEvent) => {
     event.preventDefault();
 
-    const email = emailInputRef.current;
-    const username = usernameInputRef.current;
-    const password = passwordInputRef.current;
+    if (!isEmailValid || !isUsernameValid || !isPasswordValid) return;
 
-    if (!email || !username || !password) return;
-
-    const userId = await registerUser_async(email.value, password.value);
-    const requestBody: IAddUserDto = { userId, username: username.value };
+    const userId = await registerUser_async(email, password);
+    const requestBody: IAddUserDto = { userId, username };
     await axios.post(baseApiRoute, requestBody);
+
+    navigate("/");
   };
 
   return (
@@ -32,7 +131,8 @@ const RegisterPage = () => {
         <CardContent>
           <form className="flex flex-col gap-2">
             <TextField
-              inputRef={emailInputRef}
+              value={email}
+              onChange={e => dispatchEmailAction({ payload: e.target.value, type: FormInputActionKind.userInput })}
               label='Email'
               inputProps={{
                 id: 'email',
@@ -40,9 +140,11 @@ const RegisterPage = () => {
               }}
               sx={{ width: '100%' }}
             />
+            {!isEmailValid && <p>{emailValidationError}</p>}
 
             <TextField
-              inputRef={usernameInputRef}
+              value={username}
+              onChange={e => dispatchUsernameAction({ payload: e.target.value, type: FormInputActionKind.userInput })}
               label='Username'
               inputProps={{
                 id: 'username',
@@ -50,9 +152,11 @@ const RegisterPage = () => {
               }}
               sx={{ width: '100%' }}
             />
+            {!isUsernameValid && <p>{usernameValidationError}</p>}
 
             <TextField
-              inputRef={passwordInputRef}
+              value={password}
+              onChange={e => dispatchPasswordAction({ payload: e.target.value, type: FormInputActionKind.userInput })}
               label='Password'
               inputProps={{
                 id: 'password',
@@ -60,11 +164,12 @@ const RegisterPage = () => {
               }}
               sx={{ width: '100%' }}
             />
+            {!isPasswordValid && <p>{passwordValidationError}</p>}
           </form>
         </CardContent>
 
         <CardActions sx={{ display: 'flex', flexDirection: 'column' }}>
-          <Button onClick={createUserHandler} size="small" sx={{ width: '100%' }}>
+          <Button disabled={!isFormValid} onClick={createUserHandler} size="small" sx={{ width: '100%' }}>
             Submit
           </Button>
         </CardActions>
@@ -72,6 +177,5 @@ const RegisterPage = () => {
     </div>
   );
 };
-
 
 export default RegisterPage;
